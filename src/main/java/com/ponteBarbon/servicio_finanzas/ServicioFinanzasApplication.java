8c4e5754e50488a856f3ac65c3e6e518d0e1f7f5
Brayan Smith Morales <102981505+Brays83@users.noreply.github.com>
2025-05-24 10:01:45 -0500

    chore:commit initial

---
+++ src/main/java/com/ponteBarbon/servicio_finanzas/ServicioFinanzasApplication.java
@@ -0,0 +1,13 @@
+package com.ponteBarbon.servicio_finanzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioFinanzasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioFinanzasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ponteBarbon/servicio_finanzas/ServicioFinanzasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ponteBarbon.servicio_finanzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioFinanzasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
